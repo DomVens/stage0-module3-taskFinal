ea602814e24591a5a6b6be8251392f17b28e405f
Domantas Venslovas <domantas.venslovas@stud.vilniustech.lt>
2022-08-29 14:09:43 +0300

    Hopefully finished task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum;
+        sum=number/1000+number%1000/100+number%100/10+number%10;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.finalModuleTask;
 
+import static java.lang.Integer.reverse;
+
 public class NumberReverter {
     public void revert(int number) {
+        System.out.println(reverse(number));
     }
 }
